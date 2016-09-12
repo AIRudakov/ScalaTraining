57b5aa25092fe03dc2860b4ad748f363caa697d8
AIRudakov <aliaksei_rudakov@epam.com>
2016-09-12 12:55:55 +0300

    Added printer class

---
+++ HelloWorld/src/main/scala/HelloWorld.scala
@@ -0,0 +1,9 @@
+import ar.test.StringPrinter
+
+/**
+  * Created by Aliaksei_Rudakov on 9/12/2016.
+  */
+object HelloWorld extends  App{
+      val printer=new StringPrinter
+      printer.printString("Hello World")
+}
---
+++ HelloWorld/src/main/scala/ar/test/StringPrinter.scala
@@ -0,0 +1,8 @@
+package ar.test
+
+/**
+  * Created by Aliaksei_Rudakov on 9/12/2016.
+  */
+class StringPrinter {
+    def printString (stringToPrint: String): Unit = print(stringToPrint)
+}
